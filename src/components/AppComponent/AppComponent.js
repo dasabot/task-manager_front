e6fbe3041e241ecb2c620fce66355bf0a8649734
dasabot <dasabot@icloud.com>
2023-02-18 01:42:20 +0200

    init

---
+++ src/components/AppComponent/AppComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Aside from "../Aside/Aside";
+
+export default function AppComponent() {
+    return (
+        <>
+        <Aside></Aside>
+        </>
+    )
+}
---
+++ src/components/Aside/Aside.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavButton from "../NavButton/NavButton";
+import './Aside.scss'
+
+export default function Aside() {
+    return (
+        <div>
+            <h1>ASIDE</h1>
+            <NavButton></NavButton>
+        </div>
+    )
+}
